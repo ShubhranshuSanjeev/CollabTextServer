75c8622d71a9a2d56a562ebafb96211b8104c6d2
Shubhranshu Sanjeev <shubhranshu.lumia@gmail.com>
2020-12-15 00:05:19 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const net = require('net');
 const port = 7070;
-const host = 'shielded-plateau-27399.herokuapp.com';
+const host = '127.0.0.1';
 
 const server = net.createServer();
 server.listen(port, host, () => {
